798e237526e2e347dfc06102ff608e20810df0fe
kamedashigeru <kamedashigeru@gmail.com>
2017-02-08 22:41:44 +0900

    Change db schema
    rails db:migrate:reset

--- db/migrate/20170208052829_create_users.rb
+++ db/migrate/20170208052829_create_users.rb
@@ -2,8 +2,9 @@ class CreateUsers < ActiveRecord::Migration[5.0]
   def change
     create_table :users do |t|
       t.string :email
-
+      t.string :password_digest
       t.timestamps
+      t.index :email, unique: true
     end
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170208053725) do
+ActiveRecord::Schema.define(version: 20170208052829) do
 
   create_table "users", force: :cascade do |t|
     t.string   "email"
+    t.string   "password_digest"
     t.datetime "created_at",      null: false
     t.datetime "updated_at",      null: false
-    t.string   "password_digest"
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
