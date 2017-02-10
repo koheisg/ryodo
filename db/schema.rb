0f93be6a4cc376fea35712f0ef887aa37a81551b
kamedashigeru <kamedashigeru@gmail.com>
2017-02-10 22:28:19 +0900

    Create model Article
    rails g model Article title:string content:string user:references
    migration -> add_index :microposts, [:user_id, :created_at]

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170208052829) do
+ActiveRecord::Schema.define(version: 20170210132004) do
+
+  create_table "articles", force: :cascade do |t|
+    t.string   "title"
+    t.string   "content"
+    t.integer  "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id", "created_at"], name: "index_articles_on_user_id_and_created_at"
+    t.index ["user_id"], name: "index_articles_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "email"
