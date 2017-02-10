670066cefe3cedf6e01e61f1d174133e604a8125
kamedashigeru <kamedashigeru@gmail.com>
2017-02-10 22:38:32 +0900

    Create articlescontroller
    rails g controller Articles
    method: new

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,4 @@
 class User < ApplicationRecord
-  has_many :microposts
+  has_many :microposts, dependent: :destroy
   has_secure_password
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
   get '/signup',  to: 'users#new'
   resources :users, only: :create
-  get    '/login',   to: 'sessions#new'
-  post   '/login',   to: 'sessions#create'
+  get '/login', to: 'sessions#new'
+  post '/login', to: 'sessions#create'
   get '/logout', to: 'sessions#destroy'
+  get '/posts/new', to: 'articles#new'
 end
