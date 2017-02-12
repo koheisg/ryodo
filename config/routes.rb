7e78ec5383db222e89a2844e2009ed28f8f0ff80
kamedashigeru <kamedashigeru@gmail.com>
2017-02-12 20:35:07 +0900

    Add users#edit in order to oauth github
    rails g controller Users edit

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   get '/signup',  to: 'users#new'
-  resources :users, only: :create
+  resources :users, only: [:create, :edit]
   get '/login', to: 'sessions#new'
   post '/login', to: 'sessions#create'
   get '/logout', to: 'sessions#destroy'
