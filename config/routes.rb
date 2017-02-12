0698df1f1c408c59c3523fae5be8666015b8d31f
kamedashigeru <kamedashigeru@gmail.com>
2017-02-12 19:11:07 +0900

    Create Github::AuthorizeController
    rails g controller github/authorize new callback access_token

--- config/routes.rb
+++ config/routes.rb
@@ -5,4 +5,9 @@ Rails.application.routes.draw do
   post '/login', to: 'sessions#create'
   get '/logout', to: 'sessions#destroy'
   resources :articles, except: %i(show delete)
+  namespace :github do
+    get 'authorize/new'
+    get 'authorize/callback'
+    get 'authorize/access_token'
+  end
 end
