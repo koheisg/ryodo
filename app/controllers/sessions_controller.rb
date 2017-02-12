582e572aa95cb4a13b8f0826bab09360faf946cc
kamedashigeru <kamedashigeru@gmail.com>
2017-02-12 20:35:07 +0900

    Add users#edit in order to oauth github
    rails g controller Users edit

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,6 +7,7 @@ class SessionsController < ApplicationController
     user = User.find_by(email: user_params[:email])
     if user && user.authenticate(user_params[:password])
       session[:user_id] = user.id
+      @user_id = user.id
     else
       render 'new'
     end
