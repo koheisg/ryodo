60cce1632d0c4ce06fb5b36b82819dd9505a7cc6
kamedashigeru <kamedashigeru@gmail.com>
2017-02-12 20:35:07 +0900

    Add users#edit in order to oauth github
    rails g controller Users edit

--- app/controllers/github/authorize_controller.rb
+++ app/controllers/github/authorize_controller.rb
@@ -20,9 +20,14 @@ class Github::AuthorizeController < ApplicationController
         code: params[:code]
       })
       res.body.slice!(0..12)
-      access_token = res.body
-      url = 'https://api.github.com/user?access_token=' + access_token
-      redirect_to url #コントローラーからリダイレクトさせると認証に失敗する
+      access_token = res.body #文字列として切り離しているので、このままDBに保存できる
+
+      # アクセストークンを送り返すロジック
+      # url = 'https://api.github.com/user?access_token=' + access_token
+      # redirect_to url #コントローラーからリダイレクトさせると認証に失敗する
+
+      # 今回はusers#editにリダイレクトさせて、連携完了を知らせる
+      redirect_to edit_user_path(:id => session[:user_id])
   end
 
   private
