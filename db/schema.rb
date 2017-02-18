3646def362f1924271ae4b05b7d6f00459f072c9
kamedashigeru <kamedashigeru@gmail.com>
2017-02-18 20:47:41 +0900

    Change database scheme for tags

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170215035441) do
+ActiveRecord::Schema.define(version: 20170218114026) do
 
   create_table "articles", force: :cascade do |t|
     t.string   "title"
@@ -29,6 +29,14 @@ ActiveRecord::Schema.define(version: 20170215035441) do
     t.datetime "created_at",   null: false
     t.datetime "updated_at",   null: false
     t.index ["user_id"], name: "index_github_access_tokens_on_user_id"
+  end
+
+  create_table "tags", force: :cascade do |t|
+    t.string   "tag"
+    t.integer  "article_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["article_id"], name: "index_tags_on_article_id"
   end
 
   create_table "users", force: :cascade do |t|
