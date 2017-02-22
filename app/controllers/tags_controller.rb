37954e28f5a3655b7d8440325c5973d46ab2da0b
kamedashigeru <kamedashigeru@gmail.com>
2017-02-22 11:51:32 +0900

    Create tags#new, create, index

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -1,11 +1,21 @@
 class TagsController < ApplicationController
+  before_action :verify_user
+
   def index
+    @tags = Tag.all
   end
 
   def new
+    @tag = current_user.tags.build
   end
 
   def create
+    tag = current_user.tags.build(tag_params)
+    if tag.save
+      redirect_to tags_path
+    else
+      # not yet written
+    end
   end
 
   def edit
@@ -16,4 +26,10 @@ class TagsController < ApplicationController
 
   def delete
   end
+
+  private
+
+    def tag_params
+      params.require(:tag).permit(:name)
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,4 @@
 Rails.application.routes.draw do
-  get 'tags/index'
-
-  get 'tags/new'
-
-  get 'tags/create'
-
-  get 'tags/edit'
-
-  get 'tags/update'
-
-  get 'tags/delete'
-
   get '/signup',  to: 'users#new'
   get '/me/edit', to: 'users#edit'
   resources :users, only: %i(create)
@@ -18,6 +6,7 @@ Rails.application.routes.draw do
   post '/login', to: 'sessions#create'
   get '/logout', to: 'sessions#destroy'
   resources :articles, except: %i(show delete)
+  resources :tags
   namespace :github do
     get 'authorize/new'
     get 'authorize/callback', to: 'authorize#create'
