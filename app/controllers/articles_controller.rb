b64429ef9618b90c27dd16c9af1602f23372d0dd
kamedashigeru <kamedashigeru@gmail.com>
2017-02-23 17:27:14 +0900

    Add tags to articles (saves tag only when tag is chosen)

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -7,11 +7,13 @@ class ArticlesController < ApplicationController
 
   def new
     @article = current_user.articles.build
+    @articletag = @article.article_tags.build
   end
 
   def create
     article = current_user.articles.build(article_params)
-    if article.save
+    if article.save!
+      article.article_tags.create!(tag_id: params[:article_tag][:tag_id]) # タグが選択されていない場合、現時点だと例外が発生する
       redirect_to articles_path
     else
       #not yet written
@@ -20,11 +22,30 @@ class ArticlesController < ApplicationController
 
   def edit
     @article = current_user.articles.find_by!(id: params[:id])
+    if @article.article_tags.first # 記事に中間テーブルが紐付いているかどうか
+      @articletag = @article.article_tags.all
+    else
+      @articletag = @article.article_tags.build
+    end
   end
 
   def update
     article = current_user.articles.find_by!(id: params[:id])
     if article.update_attributes(article_params)
+      if article.article_tags.first # すでにATがある場合は、値を見て更新
+        if params[:article_tags][:tag_id].empty?
+          # ない場合の記述はまだ書かない
+        else
+          article.article_tags.first.update_attributes(tag_id: params[:article_tags][:tag_id])
+        end
+      else # ない場合は、値を見て新規保存
+        if params[:article_tags][:tag_id].empty?
+          # ない場合の記述はまだ書かない
+        else
+          article.article_tags.create!(tag_id: params[:article_tag][:tag_id])
+        end
+      end
+
       redirect_to articles_path
     else
       #not yet written
