f475a34a8481149212e4593c8d44ed51d440b4b8
kamedashigeru <kamedashigeru@gmail.com>
2017-02-23 18:16:54 +0900

    Fix articlecontrollers, fix articles views

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -7,13 +7,12 @@ class ArticlesController < ApplicationController
 
   def new
     @article = current_user.articles.build
-    @articletag = @article.article_tags.build
   end
 
   def create
     article = current_user.articles.build(article_params)
-    if article.save!
-      article.article_tags.create!(tag_id: params[:article_tag][:tag_id]) # タグが選択されていない場合、現時点だと例外が発生する
+    article.article_tags.build(tag_id: params[:article_tag][:tag_id])
+    if article.save
       redirect_to articles_path
     else
       #not yet written
@@ -22,24 +21,19 @@ class ArticlesController < ApplicationController
 
   def edit
     @article = current_user.articles.find_by!(id: params[:id])
-    if @article.article_tags.first # 記事に中間テーブルが紐付いているかどうか
-      @articletag = @article.article_tags.all
-    else
-      @articletag = @article.article_tags.build
-    end
   end
 
   def update
     article = current_user.articles.find_by!(id: params[:id])
     if article.update_attributes(article_params)
       if article.article_tags.first # すでにATがある場合は、値を見て更新
-        if params[:article_tags][:tag_id].empty?
+        if params[:article_tag][:tag_id].empty?
           # ない場合の記述はまだ書かない
         else
-          article.article_tags.first.update_attributes(tag_id: params[:article_tags][:tag_id])
+          article.article_tags.first.update_attributes(tag_id: params[:article_tag][:tag_id])
         end
       else # ない場合は、値を見て新規保存
-        if params[:article_tags][:tag_id].empty?
+        if params[:article_tag][:tag_id].empty?
           # ない場合の記述はまだ書かない
         else
           article.article_tags.create!(tag_id: params[:article_tag][:tag_id])
