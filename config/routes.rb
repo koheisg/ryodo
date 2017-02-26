76b917bc33ad7379810ba188373b3a02a9eb81b0
亀田茂 <kameda@kamedashigeru-no-MacBook-Pro.local>
2017-02-26 15:06:08 +0900

    Create articles/tags#index

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,7 @@ Rails.application.routes.draw do
   get '/logout', to: 'sessions#destroy'
   resources :articles, except: %i(show delete)
   resources :tags
+  get '/articles/tags/:id', to: 'articles/tags#index', as: 'articles_tags'
   namespace :github do
     get 'authorize/new'
     get 'authorize/callback', to: 'authorize#create'
