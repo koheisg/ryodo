e82a9a5fdce506516c7b350de94ff95ecbf75566
亀田茂 <kameda@kamedashigeru-no-MacBook-Pro.local>
2017-02-26 15:41:09 +0900

    Add tags delete button in articles#edit

--- app/controllers/article_tags_controller.rb
+++ app/controllers/article_tags_controller.rb
@@ -1,2 +1,8 @@
 class ArticleTagsController < ApplicationController
+  def destroy
+    articletag = ArticleTag.find_by!(id: params[:id])
+    article_id = articletag.article_id
+    articletag.destroy
+    redirect_to edit_article_path(id: article_id)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,7 @@ Rails.application.routes.draw do
   get '/logout', to: 'sessions#destroy'
   resources :articles, except: %i(show delete)
   resources :tags
+  resources :article_tags, only: %i(destroy)
   get '/articles/tags/:id', to: 'articles/tags#index', as: 'articles_tags'
   namespace :github do
     get 'authorize/new'
