00e596e82fa299a516b0a38f6946a226a52d9576
kamedashigeru <kamedashigeru@gmail.com>
2017-02-28 13:20:07 +0900

    Add validation to model User

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,8 @@ class User < ApplicationRecord
   has_one :github_access_token
   has_one :github_repository
   has_secure_password
+  validates :email, presence: true, uniqueness: true
+  validates :password, presence: true, length: { minimum: 6 }
 
   def login?
     persisted?
