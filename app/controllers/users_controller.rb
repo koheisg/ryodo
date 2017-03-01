f56e263b774279c7947bf86b40ed93e0175614ec
kamedashigeru <kamedashigeru@gmail.com>
2017-03-01 22:36:50 +0900

    DRY userscontroller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,12 +4,11 @@ class UsersController < ApplicationController
   end
 
   def create
-    user = User.new(user_params)
-    if user.save
+    @user = User.new(user_params)
+    if @user.save
       session[:user_id] = user.id
       redirect_to articles_path
     else
-      @user = user
       respond_to do |format|
         format.html { render :new }
       end
