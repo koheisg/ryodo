65a6d680c851e67bae2cc58a4495769c4adcb7d8
fi1446 <kamedashigeru@gmail.com>
2017-03-05 17:40:07 +0900

    Change scheme User
    t.string :username

--- db/schema.rb
+++ db/schema.rb
@@ -58,9 +58,9 @@ ActiveRecord::Schema.define(version: 20170226081300) do
 
   create_table "users", force: :cascade do |t|
     t.string   "email"
-    t.string   "password_digest"
-    t.datetime "created_at",      null: false
-    t.datetime "updated_at",      null: false
+    t.string   "username"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
