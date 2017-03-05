e66f6d46f746eefc82dae70a4119bc2fea6a5497
fi1446 <kamedashigeru@gmail.com>
2017-03-05 17:40:07 +0900

    Change scheme User
    t.string :username

--- db/migrate/20170208052829_create_users.rb
+++ db/migrate/20170208052829_create_users.rb
@@ -2,7 +2,7 @@ class CreateUsers < ActiveRecord::Migration[5.0]
   def change
     create_table :users do |t|
       t.string :email
-      t.string :password_digest
+      t.string :username
       t.timestamps
       t.index :email, unique: true
     end
