469e1f6895fbbf200d5c68cdde1b76b6718ea8a1
kamedashigeru <kamedashigeru@gmail.com>
2017-03-05 12:02:10 +0900

    Change login views and sessioncontroller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,7 +5,7 @@ class SessionsController < ApplicationController
 
   def create
     user = User.find_by(email: user_params[:email])
-    if user && user.authenticate(user_params[:password])
+    if user
       session[:user_id] = user.id
       redirect_to articles_path
     else
