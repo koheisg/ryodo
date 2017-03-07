7c7979fe5598e9bbdb9589d212a29126cd939806
fi1446 <kamedashigeru@gmail.com>
2017-03-07 23:03:44 +0900

    Use helper instead of applicationcontroller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,6 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
-  helper_method :current_user, :markdown
+  helper_method :current_user
 
   def current_user
     User.find_by(id: session[:user_id]) || User::Logout.new
@@ -8,16 +8,5 @@ class ApplicationController < ActionController::Base
 
   def verify_user
     redirect_to login_path unless current_user.login?
-  end
-
-  def markdown(text)
-    markdown = Redcarpet::Markdown.new(Redcarpet::Render::HTML,
-      hard_wrap: true,
-      filter_html: true,
-      fenced_code: true,
-      gh_blockcode: true,
-      autolink: true,
-      tables: true)
-    markdown.render(text).html_safe
   end
 end
