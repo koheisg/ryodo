5d1f22d4ca71b4a361034b36445bef203b012214
fi1446 <kamedashigeru@gmail.com>
2017-03-07 13:31:40 +0900

    Fix batch Jekyll::New

--- app/batches/jekyll/new.rb
+++ app/batches/jekyll/new.rb
@@ -1,18 +1,18 @@
 class Jekyll::New < Rails::Generators::Base
   def self.run
-    if !run "cd #{Rails.root}/tmp/user_#{user.id}"
-      new.execute
-    end
+    new.execute
   end
 
   def execute
     User.all.each do |user|
       if user.github_access_token && user.github_repository
-        u_dir = "#{Rails.root}/tmp/user_#{user.id}"
-        run "cd #{u_dir} && jekyll new ./"
-        run "cd #{u_dir} && git add ."
-        run "cd #{u_dir} && git commit -m 'Jekyll new'"
-        run "cd #{u_dir} && git push origin master"
+        if !run "cd #{Rails.root}/tmp/user_#{user.id}"
+          u_dir = "#{Rails.root}/tmp/user_#{user.id}"
+          run "cd #{u_dir} && jekyll new ./"
+          run "cd #{u_dir} && git add ."
+          run "cd #{u_dir} && git commit -m 'Jekyll new'"
+          run "cd #{u_dir} && git push origin master"
+        end
       end
     end
   end
