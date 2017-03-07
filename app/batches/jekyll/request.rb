3810efdf3a94b11cf93c33df6094780e450e6b1a
fi1446 <kamedashigeru@gmail.com>
2017-03-07 14:32:35 +0900

    Integrate MarkdownExporter into Jekyll::Request

--- app/batches/jekyll/request.rb
+++ app/batches/jekyll/request.rb
@@ -12,7 +12,7 @@ class Jekyll::Request < Rails::Generators::Base
         commit_name = "Update article #{Date.today.to_s}"
         branch_name = generate_branch_name
         run "cd #{u_dir} && git checkout -b #{branch_name}"
-        # mdファイルをエクスポートするならば、ここで行う
+        export_articles(user)
         run "cd #{u_dir} && git add ."
         run "cd #{u_dir} && git commit -m '#{commit_name}'"
         run "cd #{u_dir} && git push origin #{branch_name}"
@@ -21,13 +21,33 @@ class Jekyll::Request < Rails::Generators::Base
     end
   end
 
+  def export_articles(user)
+    user.articles.each do |article|
+      u_dir = "#{Rails.root}/tmp/user_#{user.id}"
+      f = File.open("#{u_dir}/_posts/#{article.created_at.to_s.split(' ').first}-#{article.title}.md", 'w')
+      f.print front_matter(article.title)
+      f.print article.content
+      f.close
+    end
+  end
+
   private
+
     def application_name
       Rails.application.class.name.split('::').first.underscore
     end
 
     def generate_branch_name
       "#{application_name}-#{Date.today.to_s}-#{SecureRandom.uuid}"
+    end
+
+    def front_matter(title)
+      <<"EOS"
+---
+layout: post
+title: #{title}
+---
+EOS
     end
 
     def send_pull_request(user, commit, branch)
