5952b6e05f5d5dadb7c01ea26755d28ba70f2ccd
dfsa <kamedashigeru@gmail.com>
2017-03-08 10:19:58 +0900

    Show markdown into html (#61)

--- app/helpers/articles_helper.rb
+++ app/helpers/articles_helper.rb
@@ -1,2 +1,12 @@
 module ArticlesHelper
+  def markdown(text)
+    markdown = Redcarpet::Markdown.new(Redcarpet::Render::HTML,
+      hard_wrap: true,
+      filter_html: true,
+      fenced_code: true,
+      gh_blockcode: true,
+      autolink: true,
+      tables: true)
+    markdown.render(text).html_safe
+  end
 end
