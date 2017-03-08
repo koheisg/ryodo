16b8e08b805b39592f2cf7a9c5577db2461d6028
fi1446 <kamedashigeru@gmail.com>
2017-03-08 13:42:56 +0900

    Add new column status to article
    rails db:migrate:reset

--- db/schema.rb
+++ db/schema.rb
@@ -24,9 +24,10 @@ ActiveRecord::Schema.define(version: 20170226081300) do
   create_table "articles", force: :cascade do |t|
     t.string   "title"
     t.string   "content"
+    t.boolean  "status",     default: false
     t.integer  "user_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",                 null: false
+    t.datetime "updated_at",                 null: false
     t.index ["user_id"], name: "index_articles_on_user_id"
   end
 
