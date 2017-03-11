d69e1d3260687e3a909802f7ba8be51ff429eb22
fi1446 <kamedashigeru@gmail.com>
2017-03-11 18:49:55 +0900

    Fix test

--- spec/models/article_spec.rb
+++ spec/models/article_spec.rb
@@ -16,6 +16,11 @@ describe Article do
       it 'is valid with title less than 51 words' do
         expect(article_with_params).to be_valid
       end
+
+      it 'saves the article with title less than 51' do
+        article_with_barely_long_title = Article.new(user_id: 1, title: barely_long_title)
+        expect(article_with_barely_long_title.save).to be_truthy
+      end
     end
 
     context 'when given the empty params' do
@@ -37,6 +42,11 @@ describe Article do
       let(:params) { {user_id: 1, title: long_title} }
       it 'is invalid with title longer than 50 words' do
         expect(article_with_params).to be_invalid
+      end
+
+      it 'fails to save the article with title longer than 50' do
+        article_with_long_title = Article.new(user_id: 1, title: long_title)
+        expect(article_with_long_title.save).to be_falsey
       end
     end
   end
