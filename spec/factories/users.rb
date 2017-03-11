e016c5f4c83a2a0f09e8c0e507891a57230e7cb7
fi1446 <kamedashigeru@gmail.com>
2017-03-11 17:12:04 +0900

    Fix test model User using let

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,12 +1,6 @@
 FactoryGirl.define do
   factory :user do
-<<<<<<< HEAD
     username "ryodo001"
     email "ryodo@example.com"
-=======
-    id '1'
-    username "kameda"
-    email "kamedashigeru@gmail.com"
->>>>>>> Add User test creating articles
   end
 end
