37d58a3ac942e0d194208be05fb0335f67c106aa
fi1446 <kamedashigeru@gmail.com>
2017-03-11 13:27:32 +0900

    Add User test creating articles

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,19 +1,19 @@
 require 'rails_helper'
 
 describe User do
-  user = FactoryGirl.create :user
+  user = FactoryGirl.build :user
 
   describe 'validation' do
     it 'is validate' do
       expect(user).to be_truthy
     end
     it 'is not validate when username and email are blank' do
-      no_name = User.new()
-      expect(no_name.save).to be_falsey
+      no_username_nor_email = User.new()
+      expect(no_username_nor_email.save).to be_falsey
     end
     it 'is not validate when username is blank' do
-      no_name = User.new(email: 'abc@d.e')
-      expect(no_name.save).to be_falsey
+      no_username = User.new(email: 'abc@d.e')
+      expect(no_username.save).to be_falsey
     end
     it 'is not validate when email is blank' do
       no_email = User.new(username: 'kameda')
@@ -21,6 +21,19 @@ describe User do
     end
     it 'has id' do
       expect(user.id).to be_truthy
+    end
+  end
+
+  describe 'articles' do
+    it 'can create article associated with itself through proper validation' do
+      user.save
+      article = user.articles.build(title: "title")
+      expect(article.save).to be_truthy
+    end
+
+    it 'cannot create article associated with itself when title is blank' do
+      article = user.articles.build
+      expect(article.save).to be_falsey
     end
   end
 end
