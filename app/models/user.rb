f5f944d46354494bfa1a4f191732a80a301b044d
fi1446 <kamedashigeru@gmail.com>
2017-03-11 14:56:55 +0900

    Fix model User based on test

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,10 +3,6 @@ class User < ApplicationRecord
   has_many :tags
   has_one :github_access_token
   has_one :github_repository
-<<<<<<< HEAD
-=======
-  validates :username, presence: true
->>>>>>> Test model User validation
   validates :email, presence: true
 
   def login?
