c596ecf1d3ed29243f0a83a5231fbfbb4f6dab06
fi1446 <kamedashigeru@gmail.com>
2017-03-11 13:27:32 +0900

    Add User test creating articles

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,6 +1,12 @@
 FactoryGirl.define do
   factory :user do
+<<<<<<< HEAD
     username "ryodo001"
     email "ryodo@example.com"
+=======
+    id '1'
+    username "kameda"
+    email "kamedashigeru@gmail.com"
+>>>>>>> Add User test creating articles
   end
 end
