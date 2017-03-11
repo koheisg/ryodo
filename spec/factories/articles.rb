aaa6ed473b85ba398169fd5e07bfb096e5fb4563
fi1446 <kamedashigeru@gmail.com>
2017-03-11 17:57:01 +0900

    Create test files for Article
    rails g rspec:model article

--- spec/factories/articles.rb
+++ spec/factories/articles.rb
@@ -1,8 +1,12 @@
 FactoryGirl.define do
   factory :article do
+<<<<<<< HEAD
     user
     title "Title"
     content "# Hello"
     published true
+=======
+    
+>>>>>>> Create test files for Article
   end
 end
