540c43c29d8eabeb943075e1b84d8a4e3fae6704
fi1446 <kamedashigeru@gmail.com>
2017-03-11 18:13:47 +0900

    Create test model Article

--- spec/factories/articles.rb
+++ spec/factories/articles.rb
@@ -1,5 +1,8 @@
 FactoryGirl.define do
   factory :article do
-    
+    user_id 1
+    title "Title"
+    content "This is the content."
+    published true
   end
 end
--- spec/models/article_spec.rb
+++ spec/models/article_spec.rb
@@ -1,5 +1,29 @@
 require 'rails_helper'
 
-RSpec.describe Article, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+describe Article do
+  let(:article) { FactoryGirl.build :article }
+
+  describe 'Article validation' do
+    let(:article_with_no_params) { Article.new() }
+    let(:article_with_no_title) { Article.new(user_id: 1) }
+    let(:article_with_no_user_id) { Article.new(title: "Title") }
+
+    context 'passed the validation' do
+      it 'saves the article' do
+        expect(article.save).to be_truthy
+      end
+    end
+
+    context 'did not pass the validation' do
+      it 'fails to save the article with no params' do
+        expect(article_with_no_params.save).to be_falsey
+      end
+      it 'fails to save the article with no title' do
+        expect(article_with_no_title.save).to be_falsey
+      end
+      it 'fails to save the article with no user association' do
+        expect(article_with_no_user_id.save).to be_falsey
+      end
+    end
+  end
 end
