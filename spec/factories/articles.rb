c19b2cd335c02c369555a593b27acfb87b2e8bb7
fi1446 <kamedashigeru@gmail.com>
2017-03-11 18:13:47 +0900

    Create test model Article

--- spec/factories/articles.rb
+++ spec/factories/articles.rb
@@ -1,12 +1,8 @@
 FactoryGirl.define do
   factory :article do
-<<<<<<< HEAD
     user
     title "Title"
     content "# Hello"
     published true
-=======
-    
->>>>>>> Create test files for Article
   end
 end
