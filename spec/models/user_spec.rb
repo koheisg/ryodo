83ca18757d6807ac3bc6b28cca69ff869c333832
fi1446 <kamedashigeru@gmail.com>
2017-03-11 12:46:08 +0900

    Test model User validation

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,9 +1,26 @@
 require 'rails_helper'
 
 describe User do
+  user = FactoryGirl.create :user
+
   describe 'validation' do
     it 'is validate' do
-      expect(:user).to be_truthy
+      expect(user).to be_truthy
+    end
+    it 'is not validate when username and email are blank' do
+      no_name = User.new()
+      expect(no_name.save).to be_falsey
+    end
+    it 'is not validate when username is blank' do
+      no_name = User.new(email: 'abc@d.e')
+      expect(no_name.save).to be_falsey
+    end
+    it 'is not validate when email is blank' do
+      no_email = User.new(username: 'kameda')
+      expect(no_email.save).to be_falsey
+    end
+    it 'has id' do
+      expect(user.id).to be_truthy
     end
   end
 end
