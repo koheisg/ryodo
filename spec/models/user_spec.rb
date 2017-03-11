3eef67e8adb6fec658f13288f2b6f80007f84201
fi1446 <kamedashigeru@gmail.com>
2017-03-11 12:18:30 +0900

    Test User validation

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,9 @@
 require 'rails_helper'
 
-RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+describe User do
+  describe 'validation' do
+    it 'is validate' do
+      expect(:user).to be_truthy
+    end
+  end
 end
