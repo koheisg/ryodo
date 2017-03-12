e555a63cb21c38f106bd5714f052ecaa66b5abe8
fi1446 <kamedashigeru@gmail.com>
2017-03-12 15:06:08 +0900

    Fix test article

--- spec/models/article_spec.rb
+++ spec/models/article_spec.rb
@@ -17,9 +17,8 @@ describe Article do
         expect(article_with_params).to be_valid
       end
 
-      it 'saves the article with title less than 51' do
-        article_with_barely_long_title = Article.new(user_id: 1, title: barely_long_title)
-        expect(article_with_barely_long_title.save).to be_truthy
+      it 'is valid with title less than 51 words' do
+        expect(article_with_params).to be_valid
       end
     end
 
@@ -43,10 +42,13 @@ describe Article do
       it 'is invalid with title longer than 50 words' do
         expect(article_with_params).to be_invalid
       end
+    end
 
-      it 'fails to save the article with title longer than 50' do
-        article_with_long_title = Article.new(user_id: 1, title: long_title)
-        expect(article_with_long_title.save).to be_falsey
+    context 'when given the wrong params' do
+      let(:long_title) { Array.new(51){[*0..1].sample}.join }
+      let(:params) { {user_id: 1, title: long_title} }
+      it 'is invalid with title longer than 50 words' do
+        expect(article_with_params).to be_invalid
       end
     end
   end
