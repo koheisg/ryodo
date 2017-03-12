4f128f01c2b09fde3a2a806478d981f70a05b428
fi1446 <kamedashigeru@gmail.com>
2017-03-12 13:59:37 +0900

    Create test model GithubAccessToken

---
+++ spec/models/github_access_token_spec.rb
@@ -0,0 +1,22 @@
+require 'rails_helper'
+
+describe Article do
+  let(:token) { FactoryGirl.build :github_access_token }
+
+  describe '#validation' do
+    let(:token_with_params) { GithubAccessToken.new(params) }
+
+    context 'when given the correct params' do
+      it 'is valid' do
+        expect(token).to be_valid
+      end
+    end
+
+    context 'when given empty params' do
+      let(:params) { {} }
+      it 'is invalid' do
+        expect(token_with_params).to be_invalid
+      end
+    end
+  end
+end
