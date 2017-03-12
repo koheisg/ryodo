9bc623d852b261815b5d11d61ddc05aee3969d13
fi1446 <kamedashigeru@gmail.com>
2017-03-12 15:06:08 +0900

    Fix test article

--- spec/models/article_spec.rb
+++ spec/models/article_spec.rb
@@ -3,40 +3,40 @@ require 'rails_helper'
 describe Article do
   let(:article) { FactoryGirl.build :article }
 
-  describe 'Article validation' do
-    let(:article_with_no_params) { Article.new() }
-    let(:article_with_no_title) { Article.new(user_id: 1) }
-    let(:article_with_no_user_id) { Article.new(title: "Title") }
-    let(:barely_long_title) { Array.new(50){[*0..1].sample}.join }
-    let(:long_title) { Array.new(51){[*0..1].sample}.join }
+  describe '#validation' do
+    let(:article_with_params) { Article.new(params) }
 
-    context 'passed the validation' do
-      it 'saves the article' do
-        expect(article.save).to be_truthy
+    context 'when given the correct params' do
+      let(:params) { {user_id: 1, title: barely_long_title} }
+      let(:barely_long_title) { Array.new(50){[*0..1].sample}.join }
+      it 'is valid' do
+        expect(article).to be_valid
       end
 
-      it 'saves the article with title less than 51' do
-        article_with_barely_long_title = Article.new(user_id: 1, title: barely_long_title)
-        expect(article_with_barely_long_title.save).to be_truthy
+      it 'is valid with title less than 51 words' do
+        expect(article_with_params).to be_valid
       end
     end
 
-    context 'did not pass the validation' do
-      it 'fails to save the article with no params' do
-        expect(article_with_no_params.save).to be_falsey
+    context 'when given the empty params' do
+      let(:params) { {} }
+      it 'is invalid' do
+        expect(article_with_params).to be_invalid
       end
+    end
 
-      it 'fails to save the article with no title' do
-        expect(article_with_no_title.save).to be_falsey
+    context 'when given no title' do
+    let(:params) { {user_id: 1} }
+      it 'is invalid' do
+        expect(article_with_params).to be_invalid
       end
+    end
 
-      it 'fails to save the article with no user association' do
-        expect(article_with_no_user_id.save).to be_falsey
-      end
-
-      it 'fails to save the article with title longer than 50' do
-        article_with_long_title = Article.new(user_id: 1, title: long_title)
-        expect(article_with_long_title.save).to be_falsey
+    context 'when given the wrong params' do
+      let(:long_title) { Array.new(51){[*0..1].sample}.join }
+      let(:params) { {user_id: 1, title: long_title} }
+      it 'is invalid with title longer than 50 words' do
+        expect(article_with_params).to be_invalid
       end
     end
   end
