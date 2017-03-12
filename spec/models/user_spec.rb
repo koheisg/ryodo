9dfed8c31ac14b4eaa406543aae1b96e9e63d76f
fi1446 <kamedashigeru@gmail.com>
2017-03-12 13:15:04 +0900

    Fix test model User

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -4,9 +4,7 @@ describe User do
   let(:user) { FactoryGirl.create :user }
 
   describe '#validate' do
-    let(:user_with_no_params) { User.new() }
-    let(:user_with_no_username) { User.new(email: 'user001@example.com') }
-    let(:user_with_no_email) { User.new(username: 'user001') }
+    let(:user_with_params) { User.new(params) }
 
     context 'when given the correct params' do
       it 'is valid' do
@@ -14,17 +12,24 @@ describe User do
       end
     end
 
-    context 'when given the wrong params' do
-      it 'is invalid with no username and email' do
-        expect(user_with_no_params).to be_invalid
+    context 'when given no params' do
+      let(:params) { {} }
+      it 'is invalid' do
+        expect(user_with_params).to be_invalid
       end
+    end
 
-      it 'is invalid with no username' do
-        expect(user_with_no_username).to be_invalid
+    context 'when given no username' do
+      let(:params) { {email: 'user001@example.com' } }
+      it 'is invalid' do
+        expect(user_with_params).to be_invalid
       end
+    end
 
-      it 'is invalid with no email' do
-        expect(user_with_no_email).to be_invalid
+    context 'when given no email' do
+      let(:params) { {username: 'user001' } }
+      it 'is invalid' do
+        expect(user_with_params).to be_invalid
       end
     end
   end
