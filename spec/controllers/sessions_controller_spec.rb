853b6b847ae0a458790c1f3fd40e4b3429fa0ad8
亀田茂 <kameda@kamedashigeru-no-MacBook-Pro.local>
2017-03-21 14:09:09 +0900

    Add sessions_controller_spec

--- spec/controllers/sessions_controller_spec.rb
+++ spec/controllers/sessions_controller_spec.rb
@@ -1,11 +1,15 @@
 require 'rails_helper'
 
-RSpec.describe UsersController, type: :controller do
+RSpec.describe SessionsController, type: :controller do
   describe 'GET #new' do
     it 'is 302' do
       get :new
       expect(response.code).to eq("302")
     end
+  end
+
+  before do
+    User.create(email: 'kamedashigeru@gmail.com')
   end
 
   describe 'GET #create' do
@@ -14,7 +18,6 @@ RSpec.describe UsersController, type: :controller do
         params = {
           code: '8e2bf871424da25ccfef'}
         get :create, params
-        expect(User.first).to be_truthy
         expect(response).to redirect_to articles_path
       end
     end
