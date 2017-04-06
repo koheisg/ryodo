68aabdd50cf65272740b786bd5b68fb94d52abeb
dfsa <kamedashigeru@gmail.com>
2017-04-06 21:04:30 +0900

    Add pagination (#90)
    * Bundle install kaminari
    
    * Add pagination logics to articles#index, tag/articles#index

--- app/controllers/articles/tags_controller.rb
+++ app/controllers/articles/tags_controller.rb
@@ -1,6 +1,6 @@
 class Articles::TagsController < ApplicationController
   def index
     tag = current_user.tags.find_by!(id: params[:id])
-    @articles = tag.articles.all
+    @articles = tag.articles.page(params[:page])
   end
 end
