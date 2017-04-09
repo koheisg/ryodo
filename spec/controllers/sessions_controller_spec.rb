d66aeee2796d81d6de958e0db424f79dd2dce456
fi1446 <kamedashigeru@gmail.com>
2017-04-09 18:09:36 +0900

    Fix explanations

--- spec/controllers/sessions_controller_spec.rb
+++ spec/controllers/sessions_controller_spec.rb
@@ -37,20 +37,20 @@ RSpec.describe SessionsController, type: :controller do
     context 'when code is valid' do
       let(:params) { {code: '8e2bf871424da25ccfef'} }
       let(:cassette_name) { 'users_create' }
-      it 'logs in' do
+      it 'logs into the service' do
         expect(response).to redirect_to articles_path
       end
     end
     context 'when code is invalid' do
       let(:params) { {code: 'fake code'} }
       let(:cassette_name) { 'users_login_failure' }
-      it 'fails to log in' do
+      it 'fails to log into the service' do
         expect(response).to redirect_to root_path
       end
     end
   end
   describe 'GET #destroy' do
-    it 'deletes session user_id' do
+    it 'deletes session user_id and logs out the service' do
       get :destroy
       expect(response).to redirect_to root_path
     end
